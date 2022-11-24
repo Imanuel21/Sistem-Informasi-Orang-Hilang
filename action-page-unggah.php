7add7c3b62af4cf2dc7896400a760fd2e82a8960
Imanuel Mariano <82488838+Imanuel21@users.noreply.github.com>
2022-11-24 08:12:56 +0700

    Add files via upload

--- action-page-unggah.php
+++ action-page-unggah.php
@@ -8,28 +8,39 @@ $user = $_SESSION["idUser"];
 $t=time();
 $dateHariini = (date("Y-m-d",$t));
 
-$id_user = "";
+$pelapor = "";
+$id_Polisi = "";
 
-$sql0 = "SELECT Id_user FROM users WHERE username = '".$user."'";
+$sql0 = "SELECT ID_Polisi FROM missing_person_unit WHERE Username = '".$user."'";
 $result0 = $conn->query($sql0);
 if ($result0->num_rows > 0) {
   // output data of each row
   while($row0 = $result0->fetch_assoc()) {
-    $id_user = $row0['Id_user'];
+    $id_Polisi = $row0['ID_Polisi'];
   }
 }
-$_SESSION["idUser"] = $id_user;
-$polisi = $id_user;
+// $_SESSION["idUser"] = $id_user;
+// $polisi = $id_user;
 
-// untuk dapatkan ID_User
-$sql1 = "SELECT  Pelapor FROM orang_hilang WHERE No_Identitas = '".$_GET["id_hilang"]."'";
-if ($result0->num_rows > 0) {
+// untuk dapatkan yang lapor
+$No_Identitas = $_GET["id_hilang"];
+$sql1 = "SELECT  * FROM orang_hilang WHERE No_Identitas = '".$No_Identitas."'";
+$result2 = $conn->query($sql1);
+if ($result2->num_rows > 0) {
   // output data of each row
-  while($row0 = $result0->fetch_assoc()) {
-    $id_user = $row0['Id_user'];
+  while($row2 = $result2->fetch_assoc()) {
+    $pelapor = $row2['pelapor'];
   }
 }
-$sql2 = "INSERT INTO `laporan`(`No_Laporan`, `ID_User`, `No_Identitas`, `ID_Polisi`, `Tanggal_Validasi`) VALUES ('[value-1]','".$id_user."','".$_GET['id_hilang']."','".$polisi."','".$dateHariini."')";
+echo "pelapor : ".$pelapor ."<br>";
+echo "yang hilang : ".$No_Identitas ."<br>";
+echo "polisi : ".$id_Polisi ."<br>";
+echo "tanggal : ".$dateHariini ."<br>";
+$conn->close();
+include("connection-database.php");
+
+// $sql2 = "INSERT INTO laporan (`No_Laporan`, `ID_User`, `No_Identitas`, `ID_Polisi`, `Tanggal_Validasi`) VALUES ('',".$pelapor."','".$No_Identitas."','".$id_Polisi."','".$dateHariini."')";
+$sql2 = "INSERT INTO laporan (`No_Laporan`, `ID_User`, `No_Identitas`, `ID_Polisi`, `Tanggal_Validasi`) VALUES ('','".$pelapor."','".$No_Identitas."','".$id_Polisi."','".$dateHariini."')";
 
 
 if ($conn->query($sql2) === TRUE) {
