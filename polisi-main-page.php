d5be0b7678bbc94a5059b36706bbdfa7e3c1aa26
reno888 <113175405+reno888@users.noreply.github.com>
2022-12-01 11:07:16 +0700

    ubah polisi main page

--- polisi-main-page.php
+++ polisi-main-page.php
@@ -32,7 +32,7 @@ $result = $conn->query($sql1);
     <link rel="stylesheet" href="main-page.css" />
     <title>ORANG HILANG</title>
     <style>
-      .icon-button {
+  .icon-button {
   position: relative;
   display: flex;
   align-items: center;
@@ -44,6 +44,17 @@ $result = $conn->query($sql1);
   border: none;
   outline: none;
   border-radius: 50%;
+}
+.button {
+  /* margin-top: 40px; */
+    width: 100px;
+    background-color: #4169E1;
+    color: white;
+    padding: 15px 0;
+    font-size: 15px;
+    font-weight: 60;
+    border: none;
+    cursor: pointer;
 }
 
 .icon-button:hover {
@@ -57,7 +68,7 @@ $result = $conn->query($sql1);
 .icon-button__badge {
   position: absolute;
   top: -4px;
-  right: 5px;
+  right: 50px;
   width: 15px;
   height: 15px;
   background: red;
@@ -76,29 +87,17 @@ $result = $conn->query($sql1);
         <input type="text" class="search-box" placeholder="search" />
         <div class="nav-items">
         <a href="polisi-main-page.php"><img src="img/home.PNG" class="icon" alt="" /></a>
+          <!-- <a href="page-tambah-laporan.php"><img src="img/add.PNG"class="icon"/></a> -->
+
+          
+          <!-- <button type="button" class="icon-button"> -->
             <a href="polisi-page-notif.php"><img src="img/notif.png" class="icon" alt="" /></a>
+            <!-- <img src="img/notif.png" class="icon" alt="" /> -->
+          <!-- <span class="material-icons">notifications</span> -->
           <span class="icon-button__badge">1</span>
+          <!-- </button> -->
 
-          <?php
-            $sql4 = "SELECT * FROM missing_person_unit WHERE username = '".$user."'";
-            $result4 = $conn->query($sql4);
-            if ($result4->num_rows > 0) {
-              // output data of each row
-              while($row4 = $result4->fetch_assoc()) {
-              
-          ?>
-
-          <div class="dropdown">
-            <?php echo '<img style="border-radius = 50%" src= "data:image/png;base64,'.base64_encode($row4['foto_profil']).'"/> ';  ?>
-          <button class="mainmenubtn"></button>
-            <div class="dropdown-child">
-                <a href="page-login.php">LOGOUT</a>
-            </div>
-          </div>
-          <?php
-          }
-        }
-          ?>
+          <div class="icon user-profile"></div>
         </div>
       </div>
     </nav>
@@ -115,28 +114,7 @@ $result = $conn->query($sql1);
           <div class="post">
             <div class="info">
               <div class="user">
-
-                <?php $sql3 = "SELECT * FROM  users  WHERE Id_user = '".$row['pelapor']."' "; 
-                $result2 = $conn->query($sql3);
-                if ($result2->num_rows > 0) {
-                  // output data of each row
-                  while($row2 = $result2->fetch_assoc()) {
-                    $pelapor=$row2['username'];
-                    // $fotoProfil = $row2['foto_profil'];
-                    
-                    ?>
-                    <div class="profile-pic"><?php
-                      echo '<img src= "data:image/png;base64,'.base64_encode($row2['foto_profil']).'"height = "400" width ="350"/> ';
-                      ?>           
-                      " alt="" /></div>
-                    <?php
-
-                  }
-                } else {
-                    echo "Hasil Pencarian Tidak Ada";
-                }
-                ?>
-
+                <div class="profile-pic"><img src="img/PForghilang.jpeg" alt="" /></div>
                 <?php $sql3 = "SELECT * FROM  users  WHERE Id_user = '".$row['pelapor']."' "; 
                 $result2 = $conn->query($sql3);
                 if ($result2->num_rows > 0) {
@@ -162,46 +140,46 @@ $result = $conn->query($sql1);
                       echo '<img src= "data:image/png;base64,'.base64_encode($row['Foto']).'"height = "400" width ="350"/> ';
                       ?>
                     </td>
-                    <td><b>Nama</b></td>
-                    <td><b>:</b></td>
-                    <td><b><?php echo $row["Nama"]; ?></b></td>
+                    <td>Nama</td>
+                    <td>:</td>
+                    <td><?php echo $row["Nama"]; ?> </td>
                     
                   </tr>
                   <tr>
-                    <td><b>TTL</b></td>
-                    <td><b>:</b></td>
-                    <td><b><?php echo $row["Tanggal_Lahir"]; ?></b> </td>
+                    <td>TTL</td>
+                    <td>:</td>
+                    <td><?php echo $row["Tanggal_Lahir"]; ?> </td>
                   </tr>
                   <tr >
-                    <td><b>Pekerjaan</b></td>
-                    <td><b>:</b></td>
-                    <td><b><?php echo $row["Pekerjaan"]; ?></b> </td>
+                    <td>Pekerjaan</td>
+                    <td>:</td>
+                    <td><?php echo $row["Pekerjaan"]; ?> </td>
                   </tr>
                   <tr >
-                    <td><b>Agama</b></td>
-                    <td><b>:</b></td>
-                    <td><b><?php echo $row["Agama"]; ?> </b></td>
+                    <td>Agama</td>
+                    <td>:</td>
+                    <td><?php echo $row["Agama"]; ?> </td>
                   </tr>
                   <tr >
-                    <td><b>Alamat</b></td>
-                    <td><b>:</b></td>
-                    <td><b><?php echo $row["Alamat"]; ?></b> </td>
+                    <td>Alamat</td>
+                    <td>:</td>
+                    <td><?php echo $row["Alamat"]; ?> </td>
                   </tr>
                   <tr >
-                    <td><b>Ciri-ciri</b></td>
-                    <td><b>:</b></td>
-                    <td><b><?php echo $row["Ciri_ciri"]; ?></b></td>
+                    <td>Ciri-ciri</td>
+                    <td>:</td>
+                    <td><?php echo $row["Ciri_ciri"]; ?></td>
                   </tr>
                   <tr >
-                    <td><b>Deskripsi</b></td>
-                    <td><b>:</b></td>
-                    <td><b><?php echo $row["Deskripsi"]; ?></b> </td>
+                    <td>Deskripsi</td>
+                    <td>:</td>
+                    <td><?php echo $row["Deskripsi"]; ?> </td>
                   </tr>
                   </table>
             </div>
             <div class="post-content">
               <div class="reaction-wrapper">
-                <a href="edit-laporan.php?No_Identitas=<?php echo $row["No_Identitas"]?>"><button>Edit Laporan<img src="img/edit.png" class="icon" alt="" title="Edit laporan" /></button></a>
+                <a href="edit-laporan.php?No_Identitas=<?php echo $row["No_Identitas"]?>"><button class="button">Edit Laporan<img src= class="icon" alt="" title="Edit laporan" /></button></a>
                 
                 <!-- <img src="img/send.PNG" class="icon" alt="" /> -->
                 <!-- <img src="img/save.PNG" class="save icon" alt="" /> -->
